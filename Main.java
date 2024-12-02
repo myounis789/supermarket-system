64284774e78a39de9dd91bb2a582c11413807f32
myounis789 <mohammedyounis789@gmail.com>
2024-12-02 15:56:29 +0000

    Merged branches and updated main class

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
    public static void main(String[] args) {
        System.out.println("Supermarket Checkout System: ");
+       Item item1 = new Item("Apple", 1.50); 
+       Item item2 = new Item("Banana", 0.80);
+       Cart cart = new Cart();
+       cart.addItem(item1);
+       cart.addItem(item2);
+
+       double total = cart.calculateTotal();
+       System.out.println("Total Price: $" + total);
    }
 }
